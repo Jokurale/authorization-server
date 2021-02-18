eb7f46c8b45120fb7ba959b5b1a329b17f18852f
= <podsiadly.michal@protonmail.com>
2021-02-18 22:00:57 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// *** Bolier-plate code
+const express = require("express");
+const app = express();
+
+// *** Request-limiter import
+const rateLimit = require("express-rate-limit");
+
+// *** .ENV
+require("dotenv").config();
+
+// *** Request-limiter config
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 20,
+});
+
+// *** Addons
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(require("body-parser").json());
+app.use(require("morgan")("combined"));
+app.use(require("helmet")());
+app.use(limiter);
+
+// *** Routing
+app.listen(process.env.DEV_PORT || 5000, () => {
+  console.log(
+    `[Auth Service] Auth service is up and running on port ${
+      process.env.DEV_PORT || 5000
+    }.`
+  );
+});
