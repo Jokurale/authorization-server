ea2fd27668f247aab76ef195f58f1b5470a0993f
= <podsiadly.michal@protonmail.com>
2021-02-19 12:35:35 +0100

    Removed unneccesery testing data

--- routes/IndexRoute.js
+++ routes/IndexRoute.js
@@ -1,18 +1,7 @@
 const express = require("express");
 
-const TokenPayloadJoiSchema = require("../data-validators/TokenPayloadValidator");
-
 const route = express.Router();
 
-route.use("/", (req, res) => {
-  const ValdationResult = TokenPayloadJoiSchema.validate({
-    DisplayName: "Michał Podsiadły",
-    Login: "root",
-    Rank: "root",
-  });
-
-  if (ValdationResult.error) res.json({ message: "Payload invalid." });
-  else res.json(ValdationResult.value);
-});
+route.use("/", (req, res) => {});
 
 module.exports = route;
